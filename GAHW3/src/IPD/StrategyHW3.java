2da2c7d67cb34098f4628407cf03254420b108d5
chathika <chathika@knights.ucf.edu>
2015-04-11 04:20:13 -0400

    Strategies initialize randomly

--- GAHW3/src/IPD/StrategyHW3.java
+++ GAHW3/src/IPD/StrategyHW3.java
@@ -3,13 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-package IPD;
+import java.math.*;
 
 /**
  *
  * @author Chathika
  */
-public class StrategyHW3 {
+public class StrategyHW3 extends Strategy {
+    StringBuilder decisionReference; //holds the decisions to be made 
+    public StrategyHW3 () {
+        //Randomly initialize strategy
+        for(int i = 0; i < 4; i++) {
+            double r = Math.random();
+            if ( r < 0.5) {
+                decisionReference.setCharAt(i, '0');
+            } else {
+                decisionReference.setCharAt(i, '1');
+            }
+        }
+    }
     
 }
